89c80a4da13a1c64ed1504574414e92b22f81dde
bernalo-lab <bsogeke@bernalo.com>
2025-06-24 20:23:09 +0100

    Dashboard Created 20_23

--- backend/app.py
+++ backend/app.py
@@ -36,7 +36,7 @@ def global_risk_score():
             "score": result["score"],
             "confidence": result["confidence"],
             "factors": result["factors"],
-            "submitted_via_form": data.get("submitted_via_form", false)  # 👈 new flag only for form/API
+            "submitted_via_form": data.get("submitted_via_form", "false")  # 👈 new flag only for form/API
         }
 
         collection.insert_one(log_data)
