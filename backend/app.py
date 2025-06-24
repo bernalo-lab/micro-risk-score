099c655a09ba86985e2c7e24048cfc4292bcc391
bernalo-lab <bsogeke@bernalo.com>
2025-06-24 17:27:55 +0100

    Deployment & Database Connection 17_27

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,16 @@
 
 from flask import Flask, request, jsonify
+from datetime import datetime
 from flask_cors import CORS
 from scoring import calculate_risk_score
+from pymongo import MongoClient
+import os
+
+# Replace this with your actual connection string (use environment variable ideally)
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://toyinsogeke:pHCxSon6SckWubCE@financial-sandbox.haxec4g.mongodb.net/?retryWrites=true&w=majority&appName=financial-sandbox")
+client = MongoClient(MONGO_URI)
+db = client["risklogDB"]
+collection = db["submissions"]
 
 CORS(app, resources={r"/api/*": {"origins": "https://micro-risk-score.vercel.app"}}, supports_credentials=True)
 
@@ -10,6 +19,26 @@ def global_risk_score():
     try:
         data = request.get_json()
         result = calculate_risk_score(data)
+
+        log_data = {
+            "timestamp": datetime.utcnow(),
+            "first_name": data.get("first_name"),
+            "last_name": data.get("last_name"),
+            "email": data.get("email"),
+            "postcode": data.get("postcode"),
+            "country": data.get("country"),
+            "id_type": data.get("id_type"),
+            "linkedin": data.get("linkedin"),
+            "github": data.get("github"),
+            "payment_history": data.get("payment_history"),
+            "reputation_score": data.get("reputation_score"),
+            "score": result["score"],
+            "confidence": result["confidence"],
+            "factors": result["factors"]
+        }
+
+        collection.insert_one(log_data)
+
         return jsonify(result), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
