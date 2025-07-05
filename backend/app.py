58d5448e1b635bc18148145c614b7ec076546ca0
bernalo-lab <bsogeke@bernalo.com>
2025-07-05 22:18:56 +0100

    App Update - 22_18

--- backend/app.py
+++ backend/app.py
@@ -369,10 +369,9 @@ def assessment_risk_score():
 
         # Validate data
         if not data.get("submitted_By"):
-            return jsonify(
-             { "error": "Invalid 'Submitted By' Email",
-               "email": data.get("submitted_By")
-             }), 401
+            return jsonify({"error": data.get("submitted_By")}), 401
+            # return jsonify({"error": "Invalid 'Submitted By' Email"}), 401
+
 
         if not data.get("assessedEmail"):
             return jsonify({"error": "Invalid Assessed Email"}), 401
